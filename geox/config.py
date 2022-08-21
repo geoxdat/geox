860c179fadc85499c811cfdcbbee032bc3ecd991
M Razif Rizqullah <razifrizqullah@gmail.com>
2022-08-21 07:02:57 +0700

    add server option

--- geox/config.py
+++ geox/config.py
@@ -1,1 +1,7 @@
-SERVER_HOST = 'http://localhost:8000'
+import os
+
+
+if '/Users/rizquuula/Desktop/geox' == os.getcwd():
+    SERVER_HOST = 'http://localhost:8000'
+else:
+    SERVER_HOST = 'https://api.geoxdat.com'
