ac0a6a2bc7b5f9c060639230da8b61c90f7dc3d1
M Razif Rizqullah <razifrizqullah@gmail.com>
2022-08-21 07:51:38 +0700

    add env

--- geox/config.py
+++ geox/config.py
@@ -1,7 +1,6 @@
 import os
 
-
-if '/Users/rizquuula/Desktop/geox' == os.getcwd():
+if os.getenv('MODE') == 'development':
     SERVER_HOST = 'http://localhost:8000'
 else:
     SERVER_HOST = 'https://api.geoxdat.com'
